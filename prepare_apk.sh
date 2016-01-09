6c77c08793a941864d718b6071be080505cfd3c1
Krzysztof Klinikowski <kkszysiu@gmail.com>
2016-01-09 03:52:59 +0100

    Update prepare apk script

--- prepare_apk.sh
+++ prepare_apk.sh
@@ -9,10 +9,11 @@ echo "./apps/apktool d ./original_apk/mifm-1.37-1-6-3.apk --output ./mifm-1.37-1
 echo "cp ./translation/strings.xml ./mifm-1.37-1-6-3/res/values/strings.xml"
 
 ls -lath
+ls -lath ./mifm-1.37-1-6-3/
 
 cp ./translation/strings.xml ./mifm-1.37-1-6-3/res/values/strings.xml
 
-echo "./apps/apktool b mifm-1.37-1-6-3 --output ./dist/mifm-1.37-1-6-3.apk"
+echo "./apps/apktool b ./mifm-1.37-1-6-3 --output ./dist/mifm-1.37-1-6-3.apk"
 ./apps/apktool b mifm-1.37-1-6-3 --output ./dist/mifm-1.37-1-6-3.apk
 
 echo "java -jar ./apps/sign.jar ./dist/mifm-1.37-1-6-3.apk --override"
