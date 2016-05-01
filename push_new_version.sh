67254afd99a15498080e8aa4256946a9c705a136
Krzysztof <kkszysiu@gmail.com>
2016-05-01 15:56:39 +0200

    New push new version script

--- push_new_version.sh
+++ push_new_version.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
+git pull
 
 python pull_translations.py
 python propagate_translations.py
 
-git add translation/*
-git commit -m "Added new translations" translation/*
+TDATE=`date +'%Y/%m/%d %H:%M:%S:%3N'`
+MODIFIED_FILES=`git status | grep "modified:"`
+
+if [ -z "$MODIFIED_FILES" ]; then
+    git add translation/*
+    git commit -m "Added new translations" translation/*
+
+    git add translation_statistics.txt
+    git commit -m "Added new translation stats" translation_statistics.txt
+
+    git tag -a "v.$TDATE" -m "v.$TDATE"
+
+    git push origin master --tags
+else
+    echo "Nothing to commit..."
+fi
+
