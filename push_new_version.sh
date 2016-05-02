9ff1470c49085c5d632863cbd51f91c5b02509a7
Krzysztof <kkszysiu@gmail.com>
2016-05-02 12:13:07 +0200

    Add new push new version

--- push_new_version.sh
+++ push_new_version.sh
@@ -5,9 +5,13 @@ python pull_translations.py
 python propagate_translations.py
 
 TDATE=`date +'%Y/%m/%d %H:%M:%S:%3N'`
-MODIFIED_FILES=`git status | grep "modified:"`
+MODIFIED_FILES=`git status | grep 'modified:'`
+
+echo "MODIFIED_FILES $MODIFIED_FILES"
 
 if [ -z "$MODIFIED_FILES" ]; then
+    echo "Nothing to commit..."
+else
     git add translation/*
     git commit -m "Added new translations" translation/*
 
@@ -17,7 +21,5 @@ if [ -z "$MODIFIED_FILES" ]; then
     git tag -a "v.$TDATE" -m "v.$TDATE"
 
     git push origin master --tags
-else
-    echo "Nothing to commit..."
 fi
 
